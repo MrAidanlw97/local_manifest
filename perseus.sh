6ee0aa751a45fa7f37615b88bf9ca2ee12c85cfb
Aidan Warner <aidan@awarner.co.uk>
2022-07-24 17:15:21 +0100

    Added perseus.sh file to use for easy syncing without resyncing entire rom etc

---
+++ perseus.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+echo "${bold}Downloading trees for branches belonging to ${normal}$1"
+
+echo "${bold}Downloading Perseus Device Tree${normal}"
+git clone https://github.com/MrAidanlw97/android_device_xiaomi_perseus -b $1 device/xiaomi/perseus > /dev/null
+
+echo "${bold}Downloading SDM845 SOC Tree${normal}"
+git clone https://github.com/MrAidanlw97/android_device_xiaomi_sdm845-common -b $1 device/xiaomi/sdm845-common > /dev/null
+
+echo "${bold}Downloading SDM845 Kernel Tree${normal}"
+git clone https://github.com/MrAidanlw97/android_kernel_xiaomi_sdm845 -b $1 kernel/xiaomi/sdm845 > /dev/null
+
+echo "${bold}Downloading Xiaomi Vendor Tree${normal}"
+git clone https://gitlab.com/the-muppets/proprietary_vendor_xiaomi -b $1 vendor/xiaomi > /dev/null
+
+echo "${bold}Downloading Xiaomi Hardware Tree${normal}"
+git clone https://github.com/MrAidanlw97/android_hardware_xiaomi hardware/xiaomi > /dev/null
